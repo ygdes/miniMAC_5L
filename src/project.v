53c1564c832744310f28cad01c64e0a17730cf9d
YannGuidon <github@f-cpu.org>
2026-03-09 07:21:00 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -69,9 +69,9 @@ module tt_um_miniMAC (
   // Den_In0 <= DEN        sg13g2_dfrbpq_1  / 49
   (* keep *) sg13g2_dfrbpq_1 DFF_den0(.Q(Den_In0), .D(DEN), .RESET_B(INT_RESET), .CLK(clk));
   // Den_In1 <= ~Den_In0   DFF_Q   sg13g2_dfrbp_1 / 52
-  (* keep *) sg13g2_dfrbp_1 DFF_den1(.Q_N(Den_In1), .D(Den_In0), .RESET_B(INT_RESET), .CLK(clk));
+  (* keep *) sg13g2_dfrbp_1 DFF_den1(.Q_N(Den_In1), .Q(), .D(Den_In0), .RESET_B(INT_RESET), .CLK(clk));
   // Den_OK <= Den_In0 & ~Den_In1  sg13g2_and2_2
-    (* keep *) sg13g2_and2_1 Den_OK_and2(.X(Den_OK), .A(Den_In0), .B(Den_In1));
+  (* keep *) sg13g2_and2_1 Den_OK_and2(.X(Den_OK), .A(Den_In0), .B(Den_In1));
 
   dff_x9    fhw(.clk(clk), .rst(INT_RESET), .D(Din9), .Q(FirstHalfWord));                           // Always samples the input
   dffen_x18  fw(.clk(clk), .rst(INT_RESET), .D({Din9, FirstHalfWord}), .Q(FirstWord), .en(Den_OK)); // Samples only if DEN is ok
@@ -85,7 +85,7 @@ module tt_um_miniMAC (
   wire [17:0] LastWord;
   wire [8:0]  LastHalfWord, LastMSB;
 
-  or16 zo16(.A(LastWord), .X(Zero_value));   // OR the 16 LSB  (beware of C/D !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
+  or16 zo16(.A(LastWord[15:0]), .X(Zero_value));   // OR the 16 LSB  (beware of C/D !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
   (* keep *) sg13g2_dfrbpq_1 DFF_sero(.Q(Zero), .D(Zero_value), .RESET_B(INT_RESET), .CLK(clk));  // Latch & output the sum
 
   dff_x9 dffMSB(.D(LastWord[17:9]), .Q(LastMSB), .clk(clk), .rst(INT_RESET));
