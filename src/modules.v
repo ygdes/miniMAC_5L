0cc8f40b13777424ec5c708af76af3d7b7a71856
YannGuidon <github@f-cpu.org>
2026-03-11 19:18:45 +0100

    (WTF Verilog ???) modules.v

--- src/modules.v
+++ src/modules.v
@@ -7,21 +7,21 @@
 
 module fanout3(
   input  wire A,
-  output [2:0] X
+  output wire [2:0] X
 );
-
+  /*
   wire N;
   (* keep *) sg13g2_inv_1 foN(.Y(N),    .A(A));
   (* keep *) sg13g2_inv_1 fo0(.Y(X[0]), .A(N));
   (* keep *) sg13g2_inv_1 fo1(.Y(X[1]), .A(N));
   (* keep *) sg13g2_inv_1 fo2(.Y(X[2]), .A(N));
-
-//  assign X = { 3{A} };
+  */
+  assign X = { 3{A} };
 endmodule
 
 module fanout4(
   input  wire A,
-  output [3:0] X
+  output wire [3:0] X
 );
   /*
   wire N;
