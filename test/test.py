67adf11a2f23c36dbcac748db5e11aff1659f5b2
YannGuidon <github@f-cpu.org>
2026-03-14 02:10:07 +0100

    'nother typo in test.py

--- test/test.py
+++ test/test.py
@@ -173,7 +173,7 @@ async def test_project(dut):
     await reset_state(dut)  
     dut._log.info("Starting Loopback Mode")
     for x in sequence:
-      await input_parameter(x, Decode, dut)
+      await input_parameter(x, Decode+Encode, dut)
       t = await output_parameter(dut)
       # print(str(i) + " : " + bin((1 << 20) + (i ^ t)) + "  " + str(t)) # show bit difference
       print(str(x) + " : " + str(t))
