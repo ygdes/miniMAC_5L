dc01a53dae04469239c85ab2ddf7eee55f13cb4d
YannGuidon <github@f-cpu.org>
2026-03-20 18:02:58 +0100

    Update gPEAC18.v

--- src/gPEAC18.v
+++ src/gPEAC18.v
@@ -5,6 +5,15 @@
   See  https://hackaday.io/project/178998 
 */
 
+module Inverters_x4 (
+    input  wire [3:0] A,
+    output wire [3:0] Y);
+  (* keep *) sg13g2_inv_4  Amp0(.Y(Y[0]), .A(A[0]));
+  (* keep *) sg13g2_inv_4  Amp1(.Y(Y[1]), .A(A[1]));
+  (* keep *) sg13g2_inv_4  Amp2(.Y(Y[2]), .A(A[2]));
+  (* keep *) sg13g2_inv_4  Amp3(.Y(Y[3]), .A(A[3]));
+endmodule
+
 module ConstOrPass(
     input  wire [17:0] A,
     input  wire C,
