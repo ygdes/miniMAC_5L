d96d85f076385f35da25c04fd7a46eba2dc621db
YannGuidon <github@f-cpu.org>
2026-03-21 22:48:22 +0100

    gPEAC18.v: scrambler & descrambler datapaths

--- src/gPEAC18.v
+++ src/gPEAC18.v
@@ -58,26 +58,26 @@ endmodule
 module ConstModOrNeg(
     input  wire [17:0] A,
     input  wire C,
-    output wire [17:0] X  
+    output wire [17:0] Y
 );
-  (* keep *) sg13g2_nand2b_1 cstxr(.Y(X[17]), .B(A[17]), .A_N(C));
-  (* keep *) sg13g2_nand2b_1 cstxq(.Y(X[16]), .B(A[16]), .A_N(C));
-  (* keep *) sg13g2_nand2b_1 cstxp(.Y(X[15]), .B(A[15]), .A_N(C));
-  (* keep *) sg13g2_nand2b_1 cstxo(.Y(X[14]), .B(A[14]), .A_N(C));
-  (* keep *) sg13g2_nand2b_1 cstxn(.Y(X[13]), .B(A[13]), .A_N(C));
-  (* keep *) sg13g2_nand2b_1 cstxm(.Y(X[12]), .B(A[12]), .A_N(C));
-  (* keep *) sg13g2_nor2_1   cstxl(.Y(X[11]), .B(A[11]), .A(  C));
-  (* keep *) sg13g2_nor2_1   cstxk(.Y(X[10]), .B(A[10]), .A(  C));
-  (* keep *) sg13g2_nor2_1   cstxj(.Y(X[ 9]), .B(A[ 9]), .A(  C));
-  (* keep *) sg13g2_nor2_1   cstxi(.Y(X[ 8]), .B(A[ 8]), .A(  C));
-  (* keep *) sg13g2_nor2_1   cstxh(.Y(X[ 7]), .B(A[ 7]), .A(  C));
-  (* keep *) sg13g2_nand2b_1 cstxg(.Y(X[ 6]), .B(A[ 6]), .A_N(C));
-  (* keep *) sg13g2_nor2_1   cstxf(.Y(X[ 5]), .B(A[ 5]), .A(  C));
-  (* keep *) sg13g2_nor2_1   cstxe(.Y(X[ 4]), .B(A[ 4]), .A(  C));
-  (* keep *) sg13g2_nor2_1   cstxd(.Y(X[ 3]), .B(A[ 3]), .A(  C));
-  (* keep *) sg13g2_nor2_1   cstxc(.Y(X[ 2]), .B(A[ 2]), .A(  C));  
-  (* keep *) sg13g2_nand2b_1 cstxb(.Y(X[ 1]), .B(A[ 1]), .A_N(C));
-  (* keep *) sg13g2_nor2_1   cstxa(.Y(X[ 0]), .B(A[ 0]), .A(  C));
+  (* keep *) sg13g2_nand2b_1 cstxr(.Y(Y[17]), .B(A[17]), .A_N(C));
+  (* keep *) sg13g2_nand2b_1 cstxq(.Y(Y[16]), .B(A[16]), .A_N(C));
+  (* keep *) sg13g2_nand2b_1 cstxp(.Y(Y[15]), .B(A[15]), .A_N(C));
+  (* keep *) sg13g2_nand2b_1 cstxo(.Y(Y[14]), .B(A[14]), .A_N(C));
+  (* keep *) sg13g2_nand2b_1 cstxn(.Y(Y[13]), .B(A[13]), .A_N(C));
+  (* keep *) sg13g2_nand2b_1 cstxm(.Y(Y[12]), .B(A[12]), .A_N(C));
+  (* keep *) sg13g2_nor2_1   cstxl(.Y(Y[11]), .B(A[11]), .A(  C));
+  (* keep *) sg13g2_nor2_1   cstxk(.Y(Y[10]), .B(A[10]), .A(  C));
+  (* keep *) sg13g2_nor2_1   cstxj(.Y(Y[ 9]), .B(A[ 9]), .A(  C));
+  (* keep *) sg13g2_nor2_1   cstxi(.Y(Y[ 8]), .B(A[ 8]), .A(  C));
+  (* keep *) sg13g2_nor2_1   cstxh(.Y(Y[ 7]), .B(A[ 7]), .A(  C));
+  (* keep *) sg13g2_nand2b_1 cstxg(.Y(Y[ 6]), .B(A[ 6]), .A_N(C));
+  (* keep *) sg13g2_nor2_1   cstxf(.Y(Y[ 5]), .B(A[ 5]), .A(  C));
+  (* keep *) sg13g2_nor2_1   cstxe(.Y(Y[ 4]), .B(A[ 4]), .A(  C));
+  (* keep *) sg13g2_nor2_1   cstxd(.Y(Y[ 3]), .B(A[ 3]), .A(  C));
+  (* keep *) sg13g2_nor2_1   cstxc(.Y(Y[ 2]), .B(A[ 2]), .A(  C));  
+  (* keep *) sg13g2_nand2b_1 cstxb(.Y(Y[ 1]), .B(A[ 1]), .A_N(C));
+  (* keep *) sg13g2_nor2_1   cstxa(.Y(Y[ 0]), .B(A[ 0]), .A(  C));
 endmodule
 
 /* a 18-bit adder, I have no mapped/optimised version available (yet)
@@ -129,27 +129,25 @@ module gPEAC18_scrambler(
   input  wire Enable,
   input  wire Phase,
   input  wire [16:0] Message_in, // C/D bit as Message_in[8]
-  output wire [17:0] Scrambled_out // 0 < data < modulus
+  output wire [17:0] X // 0 < data < modulus
 );
 
-  wire [17:0] X;
   wire [17:0] Y;
   wire [17:0] OPM;
   wire [17:0] OPX;
   wire [17:0] OPY;
   wire [17:0] ResX;
   wire [17:0] ResY;
-  wire CinX, CinY, CoutX, CoutY;
+  wire CinX, CinY, CoutX, CoutY, EnX, EnY;
 
-  mux2_x18 mxX(.sel(Phase), .if0(Message_in), .if1(X), .res(OPM));
+  mux2_x18 mxX(.sel(Phase), .if0({1'b0, Message_in}), .if1(X), .res(OPM));
   ConstAdjOrPass AdjY(.A(Y), .C(Phase), .X(OPY));
   Add18 AddX(.A(OPM), .B(OPY), .Cin(CinX), .S(ResX), .Cout(CoutX));
-  Register_InitX RegX( .clk(clk), .rst(rst), .en(en), .D(ResX), .Q(X));  // EN à contrôler !
+  Register_InitX RegX(.clk(clk), .rst(rst), .en(EnX), .D(ResX), .Q(X));  // EN à contrôler !
 
   ConstAdjOrPass AdjX(.A(X), .C(Phase), .X(OPX));
   Add18 AddY(.A(OPX), .B(Y), .Cin(CinY), .S(ResY), .Cout(CoutY));
-  Register_InitX RegY( .clk(clk), .rst(rst), .en(en), .D(ResY), .Q(Y));  // EN à contrôler !
-
+  Register_InitX RegY(.clk(clk), .rst(rst), .en(EnY), .D(ResY), .Q(Y));  // EN à contrôler !
 endmodule
 
 ////////////////////////////////////////////////////////////////////
@@ -172,11 +170,21 @@ module gPEAC18_descrambler(
 
   wire [17:0] A;
   wire [17:0] B;
+  wire [17:0] T;
   wire [17:0] OPM;
-  wire [17:0] OPX;
-  wire [17:0] OPY1;
-  wire [17:0] OPY2;
-  wire [17:0] ResX;
-  wire [17:0] ResY;
-  wire CinX, CinY, CoutX, CoutY;
+  wire [17:0] OPB;
+  wire [17:0] OPT;
+  wire [17:0] ResA;
+  wire [17:0] ResB;
+  wire CinA, CinB, CoutA, CoutB, EnA, EnB, EnT;
+
+  mux2_x18 mxX(.sel(Phase), .if0(Scrambled_in), .if1(A), .res(OPM));
+  ConstModOrNeg(.A(B), .C(Phase), .Y(OPB));
+  Add18 AddA(.A(OPM), .B(OPB), .Cin(CinA), .S(ResA), .Cout(CoutA));
+  dffen_x18 RegA(.clk(clk), .rst(rst), .en(EnA), .D(ResA), .Q(A));  // EN à contrôler !
+ 
+  Register_InitX RegT( .clk(clk), .rst(rst), .en(EnT), .D(Scrambled_in), .Q(T));  // EN à contrôler !
+  ConstAdjOrPass AdjY(.A(T), .C(Phase), .X(OPT));
+  Add18 AddB(.A(OPT), .B(B), .Cin(CinB), .S(ResB), .Cout(CoutB));
+  Register_InitX RegB(.clk(clk), .rst(rst), .en(EnB), .D(ResB), .Q(B));  // EN à contrôler !
 endmodule
