8ca2b51af21fa1c324c045fc20232d72b9158aaf
YGDES <262647190+ygdes@users.noreply.github.com>
2026-03-28 04:30:36 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -61,7 +61,7 @@ module tt_um_miniMAC (
     .clk(clk), .rst(INT_RESET), .DEN(DEN), .Din9(Din9),
     .Din_OK(Din_OK), .FirstWord(FirstWord));
 
-  pipe_empty pipe(
+  pipe_encode_Hammer pipe(
     .clk(clk), .rst(INT_RESET),
     .Encode(Encode), .Decode(Decode),
     .Din_OK(Din_OK), .FirstWord(FirstWord),
