421790c38f444200bbc271bab9833f4e6b5a619b
YGDES <262647190+ygdes@users.noreply.github.com>
2026-03-28 15:49:51 +0100

    Update pipelines.v

--- src/pipelines.v
+++ src/pipelines.v
@@ -91,7 +91,7 @@ module pipe_encode_decode_loopback_Hammer(
 
   wire [17:0] HammerDec_result, HammerDec_operand;
   wire DecOpSel;
- (* keep *) sg13cmos5l_and2_1 AndSel(.A(Encode), .B(Decode), .Y(DecOpSel));
+  (* keep *) sg13cmos5l_and2_1 AndSel(.A(Encode), .B(Decode), .X(DecOpSel));
   mux2_x18 selOpDec( .sel(DecOpSel), .if0(FirstWord), .if1(HammerEnc_result), .res(HammerDec_operand) );
   Decode_Hamming_early Hdec(
       .clk(clk), .rst(rst), .HammEn(Din_OK),
