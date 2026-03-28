2280ade9e8ebd103149ad7c1973673f434e8f227
YGDES <262647190+ygdes@users.noreply.github.com>
2026-03-28 14:38:36 +0100

    Update pipelines.v

--- src/pipelines.v
+++ src/pipelines.v
@@ -45,3 +45,26 @@ module pipe_encode_Hammer(
 
   assign Dout_OK = Din_OK;
 endmodule
+
+module pipe_encode_decode_Hammer(
+  input wire clk,
+  input wire rst,
+  input wire Encode,
+  input wire Decode,
+  input wire Din_OK,
+  input wire [17:0] FirstWord,
+  output wire Dout_OK,
+  output wire [17:0] LastWord 
+);
+  wire _unused = &{ Decode, 1'b0};
+
+  wire [17:0] HammerEnc_result;
+  Encode_Hamming_early Henc(
+      .clk(clk), .rst(rst), .HammEn(Din_OK),
+      .HammIn(FirstWord), .HammOut(HammerEnc_result) );
+
+  // Mux
+  mux2_x18 selEnc( .sel(Encode), .if0(FirstWord), .if1(HammerEnc_result), .res(LastWord) );
+
+  assign Dout_OK = Din_OK;
+endmodule
